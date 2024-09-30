5f8a14a8c7de4acc7988d375742db3378314debc
mohammedshanan <mohammedshana12@gmail.com>
2024-09-30 21:46:38 +0300

    solved For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const arrayNew = [];
+  for (const value of array) {
+    arrayNew.push(appendString + value);
+  }
+
+  return arrayNew;
+}
